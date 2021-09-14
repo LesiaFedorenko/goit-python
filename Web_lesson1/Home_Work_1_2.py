3e03fd5f59939c00e5b773ccb8ce1289104963cf
Fedorenko Lesia <l.fedorenko@1plus1.corp>
2021-09-14 20:39:56 +0300

    Home Work1

--- Web_lesson1/Home_Work_1_2.py
+++ Web_lesson1/Home_Work_1_2.py
@@ -1,9 +1,12 @@
 class Meta(type):
 
     def __new__(mcs, class_name, base, attrs):
-        attrs['class_number'] = Meta.children_number
-        Meta.children_number += 1
+        attrs['class_number'] = mcs.children_number
+        mcs.children_number += 1
         return super(Meta, mcs).__new__(mcs, class_name, base, attrs)
+
+    # def __init__(cls, class_name, base, attrs):
+    #     super(Meta, cls).__init__(class_name, base, attrs)
 
 
 Meta.children_number = 0
@@ -21,9 +24,10 @@ class Cls2(metaclass=Meta):
 
 # print(Cls1.class_number)
 # print(Cls2.class_number)
-# print(a.class_number)
-# print(b.class_number)
 
 assert (Cls1.class_number, Cls2.class_number) == (0, 1)
 a, b = Cls1(''), Cls2('')
 assert (a.class_number, b.class_number) == (0, 1)
+
+# print(a.class_number)
+# print(b.class_number)
