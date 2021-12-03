6bcaf35654549dbf47abc1d00f76bbf9ce716650
Fedorenko Lesia <l.fedorenko@1plus1.corp>
2021-12-03 23:42:23 +0200

    HW9

--- Web_lesson9/query.py
+++ Web_lesson9/query.py
@@ -24,25 +24,4 @@ query_2=session.query(Contact).filter(Contact.birth_date > date(1976, 3, 24)).al
 for i in query_2:
     print(i.name, i.birth_date)
 
-
-# q=session.query(Contact).select_from(contact_phone).join(Contact, contact_phone.contact_id == Contact.id).join(Phone, contact_phone.phone_id==Phone.id).filter(Contact.name == 'Jennifer').all()
-# for i in q:
-#     print(i.name)
-
-# printer_makers = session.query(Product.maker).filter(Product.product_type == 'Printer').all()
-# print('All printer makers:' '\n')
-# print(printer_makers)
-#
-# pc_specific_date = session.query(PC).filter(PC.added_at > date(2021, 11, 5))
-# print(pc_specific_date)
-#
-# pc_data = session.query(PC).join(Product).filter(Product.maker == 'Apple').all()
-#
-# for computer in pc_data:
-#     print(f'PC speed is {computer.speed}')
-#
-#
-# max_pc_price = session.query(func.max(PC.speed)).all()
-# print(max_pc_price)
-
 session.close()
